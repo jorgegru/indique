b01846c3f828e019886501a38586a5ee17d9fef9
Andre Ragazzi <anlumira@gmail.com>
2019-09-20 10:41:15 -0300

    Teste delete users

---
+++ tests/App/Models/UsersModelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\App\Models;
+
+use Tests\Functional\BaseTestCase;
+
+class UsersModelTest extends BaseTestCase
+{
+    /**
+     * Test deletando usuario
+     */
+    public function testDeleteUser(){
+
+        $conteiner = $this->app()->getContainer();
+        
+        $Model = new \Project\Models\UsersModel($conteiner);
+
+        $dados["user_uuid"] = 1;
+        $this->assertTrue($Model->deleteUsers($dados));
+        $dados["user_uuid"] = 2;
+        $this->assertFalse($Model->deleteUsers($dados));
+    }
+}
