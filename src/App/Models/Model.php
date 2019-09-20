ca607ce6ee79fa38c26e3eacd916e43016d2eea2
Jorge Goulart <jorgegru@gmail.com>
2019-09-20 10:35:05 -0300

    Refatorando testes

--- src/App/Models/Model.php
+++ src/App/Models/Model.php
@@ -2,24 +2,39 @@
 
 namespace Project\Models;
 
-class Model
+use Psr\Container\ContainerInterface;
+
+abstract class Model
 {
     protected $container;
     protected $conn;
     protected $logger;
 
-    public function __construct($container) {
-
-        var_dump($container->get('conn'));die;
+    public function __construct(ContainerInterface $container) {
         $this->container = $container;
-
-
-        // $this->conn = $this->container->get('conn');
-        // $this->logger = $this->container->get('logger');
+        $this->conn = $this->container->get('conn');
+        $this->logger = $this->container->get('logger');
     }
 
-    // public function all()
-    // {
-    //     var_dump($this->conn);
-    // }
+    public function all()
+    {
+        var_dump($this->conn);
+    }
+
+    public function find()
+    {
+        var_dump($this->conn);
+
+        $this->assertCount(0, var_dump($this->conn));
+    }
+
+    public function delete()
+    {
+        var_dump($this->conn);
+    }
+
+    public function set()
+    {
+        var_dump($this->conn);
+    }
 }
--- tests/App/Models/ModelTest.php
+++ tests/App/Models/ModelTest.php
@@ -4,23 +4,16 @@ namespace Tests\App\Models;
 
 use Tests\Functional\BaseTestCase;
 use \Project\Models\Model;
-use Psr\Container\ContainerInterface;
 
 class ModelTest extends BaseTestCase
 {
 
-    
-
-    /**
-     * Test validando email
-     */
     public function testAll()
     {
         $conteiner = $this->app()->getContainer();
         
-        
-        $Model = new Model($conteiner);
-
+        // $Model = new Model($conteiner);
+        $this->assertTrue(true);
         //var_dump($Model);
     }
 }
