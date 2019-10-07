5871151c45451d440b3f2eda805ec172d83228dd
Andre Ragazzi <anlumira@gmail.com>
2019-10-07 14:46:07 -0300

    dev003 - SQL atualizado

--- migrations/2019-09-16-162748-indications.sql
+++ migrations/2019-09-16-162748-indications.sql
@@ -12,13 +12,22 @@ CREATE TABLE IF NOT EXISTS `indications` (
   `rua` VARCHAR(45) NULL,
   `numero` VARCHAR(45) NULL,
   `complemento` VARCHAR(45) NULL,
+  `status` TINYINT(1) NOT NULL,
+  `commission` TINYINT(1) NOT NULL,
   `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `service_uuid` VARCHAR(45) NOT NULL,
   `user_uuid` VARCHAR(45) NOT NULL,
   `company_uuid` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`uuid`),
   UNIQUE INDEX `cpf_cnpj_UNIQUE` (`cpf_cnpj` ASC),
+  INDEX `fk_indications_service_uuid_idx` (`service_uuid` ASC),
   INDEX `fk_indications_user_uuid_idx` (`user_uuid` ASC),
   INDEX `fk_indications_company_uuid_idx` (`company_uuid` ASC),
+  CONSTRAINT `fk_indications_service_uuid`
+    FOREIGN KEY (`service_uuid`)
+    REFERENCES `services` (`uuid`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_indications_user_uuid`
     FOREIGN KEY (`user_uuid`)
     REFERENCES `users` (`uuid`)
---
+++ migrations/2019-10-07-113746-services.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `indique`.`services` (
+    `uuid` VARCHAR(45) NOT NULL , 
+    `name` VARCHAR(45) NOT NULL , 
+    PRIMARY KEY (`uuid`))
+    ENGINE = InnoDB;
+    
