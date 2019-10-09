2af896d9ecbdd1a148840ff3e79db3e162481182
Andre Ragazzi <anlumira@gmail.com>
2019-10-09 08:27:57 -0300

    dev003 - Migrations ajuste

--- migrations/2019-09-16-162710-users.sql
+++ migrations/2019-09-16-162710-users.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `uuid` VARCHAR(45) NOT NULL,
-  `user_type` TINYINT(1) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(32) NOT NULL,
   `cpf` VARCHAR(45) NOT NULL,
-  `status` TINYINT(1) NOT NULL,
   `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `company_uuid` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`uuid`),
