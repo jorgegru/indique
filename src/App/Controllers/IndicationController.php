32b7573d4c18c30752d15b8a510ddbe0796c15e1
Andre Ragazzi <anlumira@gmail.com>
2019-10-24 16:31:43 -0300

    dev013 - Dashboard/migrations

--- src/App/Controllers/IndicationController.php
+++ src/App/Controllers/IndicationController.php
@@ -23,17 +23,18 @@ class IndicationController
         
         
             $tipo = $_SESSION['user']['user_type'];
-
+            $filtro = '';
             
             if(isset($metadata['nome']))$data[$metadata['nome']] = '%'.$metadata['valor'].'%';
             if(isset($metadata['status']))  $data['status'] = $metadata['status'];
+            if(isset($metadata['filtro']))  $filtro = $metadata['filtro'];
             
             // if(isset($metadata['status'])){   
             //     if($tipo == 1){
                     $join['users']['campo'] = 'uuid';
                     $join['users']['campo2'] = 'indications.user_uuid';
-                    $campos = "indications.*, users.name as user_name";
-                    $indication = $indicationsModel->allLikeLeftJoin($data,$join,$campos);
+                    $campos = "indications.*, users.name as user_name, users.uuid as user_uuid";
+                    $indication = $indicationsModel->allLikeLeftJoin($data,$join,$campos, $filtro);
             //     }
             //     else if($tipo == 2){
             //         $indication = $indicationsModel->allLike($data, ["user_type"=>array("3","4")]);
